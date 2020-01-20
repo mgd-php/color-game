55dfda012381d367eb3b1113915d8bf3e332a824
andrey <andrew.dudnik@gmail.com>
2020-01-20 13:52:45 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,81 @@
+(function() {
+    let currentNumber = 1;
+    const colors = [
+        'red', 'orange', 'yellow', 'green', 'skyblue', 'blue', 'purple', 'brown', 'gray', 'black',
+        'pink'
+    ];
+    const rightChoices = {
+        0: 'black', 1: 'red', 2: 'blue', 3: 'yellow', 4: 'green',
+        5: 'pink', 6: 'orange', 7: 'purple', 8: 'brown', 9: 'gray'
+    };
+
+    const successSound = new Audio('audio/success.ogg');
+    const failureSound = new Audio('audio/failure.ogg');
+    let changing = false;
+
+    const numberSpan = document.getElementById('number');
+    const colorButtons = document.querySelectorAll('#colors .color');
+
+    colorButtons.forEach(function(button) {
+        if (changing) {
+            return;
+        }
+        button.addEventListener('click', function() {
+            handleColorSelect(button.style.backgroundColor)
+        });
+    });
+
+    const handleColorSelect = (color) => {
+        if (color === getRightColorForNumber(currentNumber)) {
+            successSound.play();
+            numberSpan.style['color'] = color;
+            changing = true;
+            setTimeout(() => {
+                currentNumber++;
+                setNextQuest();
+            }, 1000);
+        } else {
+            failureSound.play();
+        }
+    };
+
+    const setNextQuest = () => {
+        numberSpan.style['color'] = 'white';
+        changeNumber(currentNumber);
+        updateColours(getRightColorForNumber(currentNumber));
+        changing = false;
+    };
+
+    const changeNumber = (number) => {
+        numberSpan.innerText = number;
+    };
+
+    const updateColours = (color) => {
+        let newColors = [color];
+        while (newColors.length < 3) {
+            const candidate = colors[Math.floor(Math.random()*colors.length)];
+            if (newColors.indexOf(candidate) === -1) {
+                newColors.push(candidate);
+            }
+        }
+
+        newColors = shuffleArray(newColors);
+        colorButtons.forEach((button, index) => {
+            button.style['backgroundColor'] = newColors[index];
+        })
+    };
+
+    const shuffleArray = (arr) => {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        return arr;
+    };
+
+    const getRightColorForNumber = (number) => {
+        return rightChoices[number % 10];
+    };
+
+    setNextQuest();
+})();
